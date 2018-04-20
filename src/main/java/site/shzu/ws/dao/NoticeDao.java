3477adeeff577637100f64740fdb8411b22b110f
wk <kinsonvip@foxmail.com>
2018-04-20 21:28:48 +0800

    新增公告相关类

---
+++ src/main/java/site/shzu/ws/dao/NoticeDao.java
@@ -0,0 +1,19 @@
+package site.shzu.ws.dao;
+
+import site.shzu.ws.model.Notice;
+
+public interface NoticeDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Notice record);
+
+    int insertSelective(Notice record);
+
+    Notice selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Notice record);
+
+    int updateByPrimaryKeyWithBLOBs(Notice record);
+
+    int updateByPrimaryKey(Notice record);
+}
